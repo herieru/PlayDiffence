69b6f28e6f641b0daa272fe14733587fcddb9d1a
herieru <herie270714@gmail.com>
2016-12-11 03:03:20 +0900

    三角形図形クラスをInterfaceから直接じゃなく、間に一つ基底クラスをかます形に変更と 様々な意味を分かるようにコメント

--- app/src/main/java/com/ahaproject/playdeffence/Geometry/Triangle.java
+++ app/src/main/java/com/ahaproject/playdeffence/Geometry/Triangle.java
@@ -1,6 +1,7 @@
 package com.ahaproject.playdeffence.Geometry;
 
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -10,31 +11,34 @@ import java.nio.FloatBuffer;
  * Created by akihiro on 2016/12/10.
  */
 
-public class Triangle implements BaseGeometry {
+public class Triangle extends C_Geometry {
 
-    //頂点シェーダー
-    public final String vertexShaderCode=
-            "attribute  vec4 vPosition;" +
-                    "void main() {" +
-                    "  gl_Position = vPosition;" +
-                    "}";
-    public final String fragmentShaderCode =
-            "precision mediump float;" +
-                    "void main() {" +
-                    "  gl_FragColor =vec4(1.0, 0.0, 0.0, 1.0);" +
-                    "}";
 
     private int shaderProgram;
 
     //コンストラクタ
     public Triangle()
     {
+        //vertexshader　　　attribute　は　頂点情報であるということの宣言
+        vertexShaderCode ="attribute  vec4 vPosition;" +
+                "void main() {" +
+                "  gl_Position = vPosition;" +
+                "}";
+        //picell shader
+        fragmentShaderCode =//mediump は　演算制度
+                "precision mediump float;" +
+                        "void main() {" +
+                        "  gl_FragColor =vec4(1.0, 0.0, 0.0, 1.0);" +
+                        "}";
+        //コンパイルしているIDをもらっている。
         int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
         int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+        //シェーダーのプログラムオブジェクト生成？
         shaderProgram = GLES20.glCreateProgram();
         GLES20.glAttachShader(shaderProgram, vertexShader);
         GLES20.glAttachShader(shaderProgram, fragmentShader);
         GLES20.glLinkProgram(shaderProgram);
+
     }
 
     @Override
@@ -45,29 +49,41 @@ public class Triangle implements BaseGeometry {
 
     @Override
     public int loadShader(int type, String shaderCode) {
+        //シェーダーオブジェクトの生成
         int shader = GLES20.glCreateShader(type);
+        //シェーダーオブジェクトとソースコードを結びつける
         GLES20.glShaderSource(shader, shaderCode);
+        //コンパイル
         GLES20.glCompileShader(shader);
         return shader;
     }
 
     @Override
     public void draw() {
+        //使うシェーダープログラム　適用
         GLES20.glUseProgram(shaderProgram);
-        int positionAttrib = GLES20.glGetAttribLocation(shaderProgram, "vPosition");
-        GLES20.glEnableVertexAttribArray(positionAttrib);
+
         float vertices[] = {
                 0.0f, 0.5f, 0.0f,//三角形の点A(x,y,z)
                 -0.5f, -0.5f, 0.0f,//三角形の点B(x,y,z)
                 0.5f, -0.5f, 0.0f//三角形の点C(x,y,z)
         };
+        //最後の４はfloat型故
         ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
         bb.order(ByteOrder.nativeOrder());
         FloatBuffer vertexBuffer = bb.asFloatBuffer();
+        //バッファの現在位置に指定されたbyteを書き込み　頂点数分書き込み
         vertexBuffer.put(vertices);
         vertexBuffer.position(0);
-        //GLES20.glVertexAttribPointer(positionAttrib,vertices.length, GLES20.GL_FLOAT, false, 0, vertexBuffer);
+
+        //シェーダープログラムの　vPositionの位置？を取得する　その値へのアクセスするためのIDが返ってくる
+        int positionAttrib = GLES20.glGetAttribLocation(shaderProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(positionAttrib);//その情報へのアクセスを有効にする。
+        //対応付け終了。
+       //GLES20.glVertexAttribPointer(positionAttrib,vertices.length, GLES20.GL_FLOAT, false, 0, vertexBuffer);
+       //関連付けている値のところにデータを渡す。
         GLES20.glVertexAttribPointer(positionAttrib, 3, GLES20.GL_FLOAT, false, 3 *4, vertexBuffer);
+        //描画の仕方指定　
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertices.length/3);
 
         GLES20.glDisableVertexAttribArray(positionAttrib);
