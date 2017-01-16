ae0cf81e31e52a37d553e73a11f7b08886b7e205
herieru <herie270714@gmail.com>
2017-01-17 02:54:54 +0900

    テクスチャクラスを作成しようとして、コンテキスト持って着なきゃいけないことに気づき、一時コンテキストクラスの作成

---
+++ app/src/main/java/com/ahaproject/playdeffence/JavaUsuful/ResourceControll/ContextHave.java
@@ -0,0 +1,34 @@
+package com.ahaproject.playdeffence.JavaUsuful.ResourceControll;
+
+import android.content.Context;
+
+/**
+ * Created by akihiro on 2017/01/17.
+ * this class singleton androidhon
+ */
+
+public class ContextHave {
+
+
+    private Context context;
+    private static ContextHave MyInstance;//自身が持っているインスタンス
+
+
+    public static ContextHave  getInstance(){return MyInstance;}
+
+    private ContextHave()
+    {
+
+    }
+
+    public boolean SetContext(Context context)
+    {
+        this.context = context;
+        return true;//仮
+    }
+    
+
+
+
+
+}
